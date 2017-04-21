7a9574603f5eee7927772e8446967eb3552d27d4
Adrian Miller <adrianjonmiller@gmail.com>
2017-04-20 17:37:55 -0700

    added products data

--- _site/assets/main.js
+++ _site/assets/main.js
@@ -22,11 +22,3 @@ Js.Dash.form = function (container) {
     });
   }, 100);
 };
-
-
-Js.Dash.mobileMenu = function (container) {
-  container.addEventListener('change', function (e) {
-    window.location = e.target.value;
-  })
-}
-;
